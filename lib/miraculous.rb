850205846ca4d517e473e00c26f1808e2ab23f72
rafaeldx7 <rafaeldx7@gmail.com>
2011-11-28 20:51:25 -0200

    add require to mass_assignment matcher file

--- lib/miraculous.rb
+++ lib/miraculous.rb
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
 require "miraculous/matchers/validations"
 require "miraculous/matchers/associations"
+require "miraculous/matchers/mass_assignment"
 require "miraculous/version"
